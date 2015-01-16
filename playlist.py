b9beb5720cc1b4651881354947bb5d74838b354f
Adam Lucansky <adamlucansky@gmail.com>
2015-01-16 17:34:44 +0100

    Compatibility issues

--- playlist.py
+++ playlist.py
@@ -1,6 +1,16 @@
 import pafy
 
-playlist = pafy.get_playlist(input("Enter playlist URL: "))
+from sys import version_info
+
+py3 = version_info[0] > 2 #creates boolean value for test that Python major version > 2
+
+if py3:
+  url = input("Enter playlist URL: ")
+else:
+  url = raw_input("Enter playlist URL: ")
+
+
+playlist = pafy.get_playlist(url)
 
 for video in playlist["items"]:
 	print video["pafy"].title, "\n"
